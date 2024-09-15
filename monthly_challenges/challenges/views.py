93491a32b5a53fa8de46684c55b46ea5141060d5
Aditya Shewale <141516636+eradityashewale@users.noreply.github.com>
2024-09-15 12:31:21 +0530

    create dynamic views function after changing months

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
-def january(request):
-    return HttpResponse ("<h1>Eat no meat for entire month !!!<h1/>")
 
 def february(request):
     return HttpResponse ("<h1> Walk for at least 20 minutes every day!!!<h1/>")
 
-def monthly_challenges(request):
-    return HttpResponse ()
+def monthly_challenges(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "<h1>Eat no mea month !!!<h1/>"
+    elif month == "february":
+        challenge_text = "<h1>Walk for at least 20 minutes every day<h1/>"
+    elif month == "march":
+        challenge_text = "<h1>Learn Django for at least 20 minutes every day<h1/>"
+    else:
+        return HttpResponseNotFound("<h1>This month is not supported<h1/>")
+    return HttpResponse (challenge_text)
