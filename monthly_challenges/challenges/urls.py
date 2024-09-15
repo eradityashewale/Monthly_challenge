8129b7ab0ea86e6317ee4d8770db77cc0c6fb447
Aditya Shewale <141516636+eradityashewale@users.noreply.github.com>
2024-09-15 11:08:43 +0530

    Added January and february challenge in function based views

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index)
+    path("january", views.january),
+    path("february", views.february)
 ] 
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse ("Welcome!!!")
+def january(request):
+    return HttpResponse ("<h1>Eat no meat for entire month !!!<h1/>")
+
+def february(request):
+    return HttpResponse ("<h1> Walk for at least 20 minutes every day!!!<h1/>")
